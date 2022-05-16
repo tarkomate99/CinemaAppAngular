456bc4f5e384a12084fd98a8e85f9b503f1347ff
h985712 <h985712@stud.u-szeged.hu>
2022-05-16 03:14:52 +0200

    nav, login, register added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBYQL8DaXV8BlWPvJA27ANncBeIfrYAUKg",
+    authDomain: "cinemaapp-f8ecd.firebaseapp.com",
+    projectId: "cinemaapp-f8ecd",
+    storageBucket: "cinemaapp-f8ecd.appspot.com",
+    messagingSenderId: "438975219010",
+    appId: "1:438975219010:web:607b340cf856e18a02b4db",
+    measurementId: "G-FK3Q9Q0SN8"
+  }
+
 };
 
 /*
